21de156c76c66641335b04f70c6198a4cad847ac
Keith Hendry <keithhendry@users.noreply.github.com>
2016-06-17 18:03:44 -0400

    Commented out some test code so that the project compiles

--- proton-users/src/main/scala/proton/users/Models.scala
+++ proton-users/src/main/scala/proton/users/Models.scala
@@ -3,10 +3,10 @@ package proton.users
 import slick.lifted.Tag
 import slick.model.Table
 
-class Users(tag: Tag) extends Table[(String, Double)](tag, "COFFEES") {
-  def name = column[String]("COF_NAME", O.PrimaryKey)
-
-  def price = column[Double]("PRICE")
-
-  def * = (name, price)
-}
+//class Users(tag: Tag) extends Table[(String, Double)](tag, "COFFEES") {
+//  def name = column[String]("COF_NAME", O.PrimaryKey)
+//
+//  def price = column[Double]("PRICE")
+//
+//  def * = (name, price)
+//}
